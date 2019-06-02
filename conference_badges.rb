c920db2fc95e7a66f302b6fc2a579bdd903d363b
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 23:42:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   badge_maker = "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  batch_badge_creator = badges
-  badges.each do |attendees|
-    puts "Hello, my name is #{attendees}"
+  attendees.map do |attendee|
+    "Hello, my name is #{attendee}"
 end
