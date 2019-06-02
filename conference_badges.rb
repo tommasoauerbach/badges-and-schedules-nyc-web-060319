0de8471f7f0d1cdf317bd7ace9249ec5de42b169
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 23:52:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,3 +15,13 @@ def assign_rooms(attendees)
     "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
   end
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
+  end
+end
