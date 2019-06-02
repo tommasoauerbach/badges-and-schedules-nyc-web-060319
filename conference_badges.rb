3aff54997576817cf3da5c69281dc7de4de988b3
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 23:08:25 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  badge_maker = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  batch_badge_creator = badges
+  badges.each do |attendees|
+    puts "Hello, my name is #{attendees}"
+end
